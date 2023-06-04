12517b134106781987330990eee16ad0662159a4
arhip14 <kruvdyk1503@gmail.com>
2023-06-04 11:04:38 +0300

    1

--- task7.js
+++
@@ -1,9 +0,0 @@
-const input = document.getElementById("font-size-control");
-const span = document.getElementById("text");
-input.addEventListener("input", handleInputRange);
-function handleInputRange(event) {
-  span.style.fontSize = event.currentTarget.value + "px";
-}
-
-
-
